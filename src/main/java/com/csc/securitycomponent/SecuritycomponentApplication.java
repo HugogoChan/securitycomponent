627dc3129ba35d8f973b4b70b8027de06b6c9d29
chenshaicheng <chshaicheng@tianan-insurance.com>
2022-07-22 15:47:37 +0800

    feature:新增代码

---
+++ src/main/java/com/csc/securitycomponent/SecuritycomponentApplication.java
@@ -0,0 +1,13 @@
+package com.csc.securitycomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritycomponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritycomponentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csc/securitycomponent/SecuritycomponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc.securitycomponent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritycomponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
